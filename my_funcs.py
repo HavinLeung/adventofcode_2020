5a411e966fd3bd6553c1e1fcf3d57cb69aa7b5cb
HavinLeung <havinleung98@gmail.com>
2021-02-10 12:40:05 -0500

    day7

---
+++ my_funcs.py
@@ -0,0 +1,14 @@
+# shadows generators for eager functions
+
+def lmap(f, x):
+    return list(map(f,x))
+
+def lfilter(f, x):
+    return list(filter(f,x))
+
+def count(f, x):
+    return len([_ for _ in filter(f,x)])
+
+def iter(f, x):
+    list(map(f,x))
+    return None
